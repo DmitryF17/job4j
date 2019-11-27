a453f19f9fb78e5b8dc0dc5fd0e4053169500f6e
DmitryF17 <farodmin@yandex.ru>
2019-11-28 00:10:50 +0300

    [#199587]

--- chapter_001/src/main/java/ru.job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru.job4j/loop/Counter.java
@@ -2,14 +2,12 @@ package ru.job4j.loop;
 
 public class Counter {
     public static int add(int start, int finish) {
-             int sum=0;
              int s=0;
-        for (int N = start; N <= finish; N++) {
-            if (N % 2 == 0) {
-                s = s + N;
+        for (int n = start; n <= finish; n++) {
+            if (n % 2 == 0) {
+                s = s + n;
             }
-            sum=s;
         }
-        return sum;
+        return s;
     }
 }
