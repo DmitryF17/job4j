e8694a971cbc1e151224e1b60ef99ce57cd7048c
DmitryF17 <farodmin@yandex.ru>
2019-11-27 23:48:25 +0300

    [#199587]

--- chapter_001/src/test/java/ru.job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru.job4j/loop/CounterTest.java
@@ -6,7 +6,7 @@ package ru.job4j.loop;
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
-        int result = Counter.add(1, 10);
+        int result = Counter.add(1,10);
         assertThat(result, is(30));
 
     }
