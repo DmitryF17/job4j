ae3928deb9f1f5d9fb02e049144a13f31318efaa
DmitryF17 <farodmin@yandex.ru>
2019-11-28 20:50:19 +0300

    [#199587]

--- chapter_001/src/main/java/ru.job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru.job4j/loop/Factorial.java
@@ -2,12 +2,10 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
-        int result = 1;
         int res=1;
         for(int A=1;A<=n;A++){
             res=res*A;
-           result=res;
         }
-        return result;
+        return res;
     }
 }
