94c001a7f8a46de2efb11848035467090e0ae6f6
DmitryF17 <farodmin@yandex.ru>
2019-11-28 22:04:55 +0300

    [#199594]

---
+++ chapter_001/src/main/java/ru.job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public boolean check(int finish) {
+        boolean prime = true;
+        for(int a=2;a<finish;a++) {
+            if (finish % a == 0) prime = false;
+        }
+        return prime;
+    }
+}
