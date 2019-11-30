1326e113052073a9ad6597e17bca6d786233a2a0
DmitryF17 <farodmin@yandex.ru>
2019-11-30 12:59:32 +0300

    [#199592]

--- chapter_001/src/main/java/ru.job4j/loop/NewPN.java
+++ chapter_001/src/main/java/ru.job4j/loop/NewPN.java
@@ -4,13 +4,16 @@ public class NewPN<count> {
 
     public int calc(int finish) {
 
-        CheckPrimeNumber prime= new CheckPrimeNumber();
+        CheckPrimeNumber prime = new CheckPrimeNumber();
 
         int count = 1;
-        for(int x=3;x<=finish;x++){
-            boolean a=prime.check(x);
-          if(a==true) count++;
+        for (int x = 3; x <= finish; x++) {
+            boolean a = prime.check(x);
+            if (a == true) {
+                count++;
             }
-        return count;
+        }
+            return count;
+
     }
 }
