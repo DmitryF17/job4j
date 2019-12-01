9fa1c7d94c5b36ce20ceb7b1ef2874f9e95a3dcd
DmitryF17 <farodmin@yandex.ru>
2019-12-01 12:20:40 +0300

    [#199589]

--- chapter_001/src/main/java/ru.job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru.job4j/calculator/Calculator.java
@@ -7,22 +7,51 @@ package ru.job4j.calculator;
 *@since 0.1
 */
 public class  Calculator {
+
+    /**
+     * Metod add.
+     * @param first
+     * @param second
+     */
     public static void add(double first,double second){
         double result=first+second;
         System.out.println(first+"+"+second+"="+result);
     }
+
+    /**
+     * Metod div.
+     * @param first
+     * @param second
+     */
     public static void div(double first,double second){
         double result=first/second;
         System.out.println(first+"/"+second+"="+result);
     }
+
+    /**
+     * Metod multiply.
+     * @param first
+     * @param second
+     */
     public static void multiply(double first,double second){
         double result=first*second;
         System.out.println(first+"*"+second+"="+result);
     }
+
+    /**
+     * Metod subtrack.
+     * @param first
+     * @param second
+     */
     public static void subtrack(double first,double second){
         double result=second-first;
         System.out.println(second+"-"+first+"="+result);
     }
+
+    /**
+     * Metod main
+     * @param args
+     */
     public static void main(String[] args){
         add(1,1);
         div(4,2);
--- chapter_001/src/main/java/ru.job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru.job4j/calculator/package-info.java
@@ -4,3 +4,27 @@
  *@version $id$
  *@since 0.1
  */
+/**
+ * Metod add.
+ * @param first
+ * @param second
+ */
+/**
+ * Metod div.
+ * @param first
+ * @param second
+ */
+/**
+ * Metod multiply.
+ * @param first
+ * @param second
+ */
+/**
+ * Metod subtrack.
+ * @param first
+ * @param second
+ */
+/**
+ * Metod main
+ * @param args
+ */
