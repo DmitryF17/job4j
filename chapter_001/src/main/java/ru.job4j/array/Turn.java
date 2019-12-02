1566e8039e1cd7aa871760b62c9e7b35ccd1027a
DmitryF17 <farodmin@yandex.ru>
2019-12-02 18:45:50 +0300

    [#199607]

--- chapter_001/src/main/java/ru.job4j/array/Turn.java
+++ chapter_001/src/main/java/ru.job4j/array/Turn.java
@@ -2,12 +2,10 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] input) {
-            for (int index = 0; index != input.length; index++) {
+            for (int index = 0; index < input.length/2; index++) {
                 int temp = input[index];
                 input[index] = input[input.length-(index+1)];
                 input[input.length-(index+1)] = temp;
-                if(index==input.length/2-1)
-                    break;
             }
             return input;
         }
