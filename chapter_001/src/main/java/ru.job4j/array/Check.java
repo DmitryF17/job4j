06722f02afb6bcebc285580632c5f1bad1a1f180
DmitryF17 <farodmin@yandex.ru>
2019-12-03 18:09:44 +0300

    [#199601]

--- chapter_001/src/main/java/ru.job4j/array/Check.java
+++ chapter_001/src/main/java/ru.job4j/array/Check.java
@@ -2,12 +2,14 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
-        for (int index = 0; index < data.length-1; index++) {
-            if (data[0] != data[index + 1]) break;
-                else result=true;
+        boolean result = true;
+        for (int index = 0; index < data.length - 1; index++) {
+            if (data[0] != data[index + 1]) {
+                result = false;
+                break;
             }
-
+        }
             return result;
+
     }
 }
