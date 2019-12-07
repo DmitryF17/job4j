eec8c4fdea1d19aa45ccfd0aad1cf8292e857ebf
DmitryF17 <farodmin@yandex.ru>
2019-12-07 22:45:34 +0300

    6.8. Выигрышные комбинации в сокобан [#199602]

--- chapter_001/src/main/java/ru.job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru.job4j/array/MatrixCheck.java
@@ -13,24 +13,23 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            int rw = 0; //подсчёт в рядах символа 'X'
-            int cw = 0; //подсчёт в колонках символа 'Х'
-            if (board[i][i] == 'X') {
-                for (int j = 0; j < board.length; j++) {
-                    if (board[i][j] == 'X') {
-                        rw++;
-                    }
-                    if (board[j][i] == 'X') {
-                        cw++;
-                    }
-                }
-            }
-            if (rw == board.length || cw == board.length) {
-                result = true;
-                break;
-            }
+            int rw = 0; // количество в рядах символа 'X'
+            int cw = 0; // количество в колонках символа 'Х'
+                 if (board[i][i] == 'X') { // поиск возможных линий
+                     for (int j = 0; j < board.length; j++) {
+                         if (board[i][j] == 'X') { // подсчет количества
+                             rw++;                 // символа 'X' в ряду
+                         }
+                         if (board[j][i] == 'X') { // подсчет количества
+                             cw++;                 // символа 'X' в колонке
+                         }
+                     }
+                         if (rw == board.length || cw == board.length) { // условие выигрыша
+                             result = true;
+                             break; // прерывание цикла при выполнении условия выигрыша
+                         }
+                 }
         }
-        System.out.println();
         return result;
 
     }
