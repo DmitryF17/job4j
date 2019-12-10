5f477b643aac283ea823fb9282a2d42d6d52fa1d
DmitryF17 <farodmin@yandex.ru>
2019-12-10 22:56:31 +0300

    1.Экзамен .Объединить два массива [#124482]

---
+++ chapter_001/src/main/java/ru.job4j/array/Merge.java
@@ -0,0 +1,63 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int l = 0;
+        int r = 0;
+        int t = 0;
+        if(left.length == 0){
+            while(r<right.length){
+                rsl[t] = right[r];
+                r++;
+                t++;
+            }
+        }
+        if(right.length == 0){
+            while(l<left.length) {
+                rsl[t] = left[l];
+                l++;
+                t++;
+            }
+        }
+        while (l < left.length && r < right.length) {
+           if(left[l] <= right[r]){
+               rsl[t] = left[l];
+                   l++;
+               }
+           else {
+               rsl[t] = right[r];
+               r++;
+               }
+            t++;
+           if(l>=left.length){
+               while(r<right.length){
+                   rsl[t] = right[r];
+                   r++;
+                   t++;
+               }
+           }
+            if(r>=right.length){
+                while(l<left.length){
+                    rsl[t] = left[l];
+                    l++;
+                    t++;
+                }
+            }
+
+
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
