c6183e29ee22cfd1a5ad60d2387d8e376c82e2d8
DmitryF17 <farodmin@yandex.ru>
2019-12-18 20:09:17 +0300

    2.2. Рефакторинг - Площадь треугольника.[#199722]

--- chapter_001/src/main/java/ru.job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru.job4j/condition/Triangle.java
@@ -1,6 +1,39 @@
 package ru.job4j.condition;
+import static java.lang.Math.sqrt;
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-       return  ab + ac > bc && ab + bc > ac && ac + bc > ab;
+    private Point first;
+    private Point second;
+    private Point third;
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
     }
- }
+    public double period(double a, double b, double c) {
+        if(this.exist(a, b, c)) {
+           double p = (a + b + c) / 2;
+        }
+        return -1;
+    }
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double rsl = -1;
+        Point first = new Point(x1, y1);
+        Point second = new Point(x2, y2);
+        Point third = new Point(x3, y3);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        else rsl = -1;
+        return rsl;
+    }
+    public static boolean exist(double a, double c, double b) {
+        if(a + b == c | a + c == b | c + b == a){
+            return false;
+        }
+            return true;
+    }
+}
