92cbd914fe222d4ec17011dd5a75e4e8720d18e2
DmitryF17 <farodmin@yandex.ru>
2019-12-24 20:59:04 +0300

    5. Аннотация @Override[#199741]

--- chapter_001/src/main/java/ru/job4j/tracker/JSONReport.java
+++ chapter_001/src/main/java/ru/job4j/tracker/JSONReport.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return name + "name" + body + "body";
     }
--- chapter_001/src/main/java/ru/job4j/tracker/TextReport.java
+++ chapter_001/src/main/java/ru/job4j/tracker/TextReport.java
@@ -1,5 +1,5 @@
 package ru.job4j.tracker;
-public class TextReport {
+ public class TextReport {
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
