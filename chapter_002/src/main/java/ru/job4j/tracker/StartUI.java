0d3f2dd6b9be2136a94a01ba35ebbd1c00e3d41d
DmitryF17 <farodmin@yandex.ru>
2019-12-29 21:28:57 +0300

    2.1. Реализация класса StartUI[#199697]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+import java.util.Scanner;
+public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        int run = 0;
+        while (run != 1) {
+            this.showMenu();
+            System.out.println("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+				System.out.println("Show all items ");
+				Item[] item = tracker.findAll();
+				System.out.println(item);
+			} else if (select == 2) {
+				System.out.println("Edit item ");
+			} else if (select == 3) {
+                System.out.println("Delete item ");
+            } else if (select == 4) {
+                System.out.println("Find item by id");
+				System.out.print("Enter id");
+				String id = scanner.nextLine();
+				Item item = tracker.findById(id);
+				System.out.println(item);
+            } else if (select == 5) {
+                System.out.println("Find items by name");
+				System.out.print("Enter name");
+				String name = scanner.nextLine();
+				Item[] item = tracker.findByName(name);
+				System.out.println(item);
+            } else if (select == 6) {
+                System.out.println("Exit Program ");
+				run = 1;
+            }
+        }
+    }
+        private void showMenu() {
+            System.out.println("Menu. ");
+            System.out.println("0. Add new Item ");
+            System.out.println("1. Show all items ");
+            System.out.println("2. Edit item ");
+            System.out.println("3. Delete item ");
+            System.out.println("4. Find item by Id ");
+            System.out.println("5. Find items by name ");
+            System.out.println("6. Exit Program ");
+        }
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Tracker tracker = new Tracker();
+            new StartUI().init(scanner, tracker);
+        }
+
+}
