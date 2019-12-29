ff43b022beadb1565349e24cd8f5fb86d764d61f
DmitryF17 <farodmin@yandex.ru>
2019-12-29 12:30:41 +0300

    2. Scanner и чтение числа из консоли.[#199702]

--- chapter_002/src/main/java/ru/job4j/io/Mathes.java
+++ chapter_002/src/main/java/ru/job4j/io/Mathes.java
@@ -1,15 +1,24 @@
 package ru.job4j.io;
+
 import java.util.Scanner;
+
 public class Mathes {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int player = 0;
         int result = 11;
         while (result > 0) {
             System.out.print("player take ");
             int math = Integer.valueOf(input.nextLine());
             result -= math;
+            player++;
             System.out.println("on the table " + result);
         }
-        System.out.print("Game Over");
+        if (player % 2 == 0) {
+            System.out.println("Win the second player");
+        } else {
+            System.out.println("Win the first player");
+        }
     }
 }
