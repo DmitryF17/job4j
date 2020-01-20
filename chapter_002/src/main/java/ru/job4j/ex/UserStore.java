16f7aaa208186324553bb1fce8991157f88a22b3
DmitryF17 <farodmin@yandex.ru>
2020-01-20 21:38:12 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- chapter_002/src/main/java/ru/job4j/ex/UserStore.java
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -1,46 +1,44 @@
 package ru.job4j.ex;
 
+
 public class UserStore {
 
+    public static boolean validate(User user) throws UserInvalidException {
+
+        if (us == null || user.length() < 3) {
+            throw  new UserInvalidException("name not validate");
+        }
+        return false;
+    }
+
     public static User findUser(User[] users, String login) throws UserNotFoundException {
+
         int rsl = -1;
-        User us = null;
         for (int i = 0; i < users.length; i++) {
             if (users[i].equals(login)) {
-                us = users[i];
+                rsl = i;
                 break;
             }
         }
         if (rsl == -1) {
             throw new UserNotFoundException("login not found");
         }
-        return us;
+        return null;
     }
 
-    public static boolean validate(User user) throws UserInvalidException {
 
-        if (user.getUsername() == "Petr") {
-            return true;
-        }
-        System.out.println("login do not validate");
-        return false;
-    }
+
 
     public static void main(String[] args) throws UserNotFoundException, UserInvalidException {
-        User[] users = {
-                new User("Petr", true)
-        };
-        try {
-            findUser(users, "Petr");
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-        }
+        User[] users = {new User("Petr", true)};
         User user = findUser(users, "Petr");
-        try {
-            if (validate(user)) {
+            try {
+                if (validate(user)) {
                 System.out.println("This user has an access");
             }
-        } catch (UserInvalidException e) {
+        } catch (UserInvalidException et) {
+            et.printStackTrace();
+        } catch (UserNotFoundException e) {
             e.printStackTrace();
         }
     }
