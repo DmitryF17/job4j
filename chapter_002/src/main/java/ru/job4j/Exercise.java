f3b71f0f29b1a518f38ba2092fd2038c876c7d50
DmitryF17 <farodmin@yandex.ru>
2020-03-13 17:05:29 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#199547]

--- chapter_002/src/main/java/ru/job4j/Exercise.java
+++ chapter_002/src/main/java/ru/job4j/Exercise.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class Exercise {
-        public static void main(String[] args) {
-            System.out.println(args.length);
-        }
+    public static int max(int left, int right) {
+        return left < right ? left : right;
+    }
     }
 
--- chapter_002/src/test/java/ru/job4j/jdknew/StudentTest.java
+++ chapter_002/src/test/java/ru/job4j/jdknew/StudentTest.java
@@ -3,6 +3,8 @@ package ru.job4j.jdknew;
 import org.junit.Test;
 
 import java.util.List;
+
+
 import static org.junit.Assert.*;
 
 import static ru.job4j.jdknew.Student.levelOf;
@@ -11,11 +13,11 @@ public class StudentTest {
     @Test
     public void levelMoreThan3() {
       List<Student> students = List.of(
-              (new Student("Ivanov", 5)),
-              (new Student("Petrov", 2)),
-              (new Student("Fedorov", 1))
+              (new Student("Ivanov", 1)),
+              (new Student("Petrov", 5)),
+              (new Student("Fedorov", 2))
       );
-      var res = new Student("Ivanov", 5);
+      var res = new Student("Petrov", 5);
       List<Student> rsl = levelOf(students, 3);
       assertEquals(rsl, List.of(res));
     }
