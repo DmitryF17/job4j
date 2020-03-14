6e6365a045b20367fad0cae75b4432715f5107e5
DmitryF17 <farodmin@yandex.ru>
2020-03-14 12:22:08 +0300

    0. Stream API улучшения[#199555]

--- chapter_002/src/main/java/ru/job4j/jdknew/Student.java
+++ chapter_002/src/main/java/ru/job4j/jdknew/Student.java
@@ -1,11 +1,9 @@
 package ru.job4j.jdknew;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 public class Student {
     private String name;
     private int score;
@@ -25,6 +23,23 @@ public class Student {
 
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream().flatMap(Stream::ofNullable).sorted(new Sortedstudents()).takeWhile(sc -> sc.getScore() > bound).collect(Collectors.toList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Student student = (Student) o;
+            return Objects.equals(student.name, score);
     }
 }
 
--- chapter_002/src/test/java/ru/job4j/jdknew/StudentTest.java
+++ chapter_002/src/test/java/ru/job4j/jdknew/StudentTest.java
@@ -2,6 +2,7 @@ package ru.job4j.jdknew;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -11,14 +12,14 @@ import static ru.job4j.jdknew.Student.levelOf;
 public class StudentTest {
     @Test
     public void levelMoreThan3() {
-        List<Student> students = List.of(
+        List<Student> students = Arrays.asList(
                 (new Student("Ivanov", 1)),
-                (new Student("Petrov", 5)),
                 null,
+                (new Student("Petrov", 5)),
                 (new Student("Sidorov", 2))
         );
         List<Student> rsl = levelOf(students, 3);
         Student exp = new Student("Petrov", 5);
-        assertThat(rsl, is(exp));
+        assertThat(rsl.get(0), is(exp));
     }
 }
