77c915f2d010a99068e5520cf658b848df7b5e35
DmitryF17 <farodmin@yandex.ru>
2020-09-14 00:07:47 +0300

    4.5. Travis CI и PostgreSQL [#259692]

--- chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SqlTrackerTest {
